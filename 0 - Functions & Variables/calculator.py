d5d7c9094ad03b14d0bfe5dc2f145930f6ba40a7
Majesty Yolo <145739768+Octo-Data-Scientist@users.noreply.github.com>
2025-07-16 21:18:40 +0100

    Add files via upload

---
+++ 0 - Functions & Variables/calculator.py
@@ -0,0 +1,18 @@
+# x = int(input("What's x? "))
+# y = int(input("What's y? "))
+
+# z = x + y
+
+# print(z)
+
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+# z = round(x / y, 2)
+
+# z = round(x + y)
+
+# print(f"{z:,}")
+
+z = x / y
+print(f"{z:.2f}")
