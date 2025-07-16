96c837b63c7216f7ac4398516f7d38e157e96ad8
Majesty Yolo <145739768+Octo-Data-Scientist@users.noreply.github.com>
2025-07-16 21:20:59 +0100

    Add files via upload

---
+++ 1 - Conditionals/grade.py
@@ -0,0 +1,21 @@
+
+score = int(input("Score: "))
+
+if 80 <= score <= 100:
+    print("Grade: A")
+
+elif 70 <= score < 80:
+    print("Grade B")
+
+elif 60 <= score < 70:
+    print("Grade C")
+
+elif 50 <= score < 60:
+    print("Grade D")
+
+elif 40 <= score < 50:
+    print("Grade E")
+    
+else:
+    print("Grade F")
+
